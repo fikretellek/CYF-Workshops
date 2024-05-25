aec9fe5b27220713585bac345ce3ecc06370c022
fikretellek <fikretellek@icloud.com>
2024-05-25 13:54:13 +0100

    something done

--- node-challenge-london-mini-guide/CYF-React-Express-Template/src/server/index.js
+++ node-challenge-london-mini-guide/CYF-React-Express-Template/src/server/index.js
@@ -1,10 +1,26 @@
 import express from "express";
+import stratford from "../../../data/Stratford.json" with { type: "json" };
+import harrow from "../../../data/Harrow.json" with { type: "json" };
+import heathrow from "../../../data/Heathrow.json" with { type: "json" };
+
+const cities = {stratford, harrow, heathrow}
 
 const app = express();
-app.listen(4173, () => {
-  console.log("Listening on http://localhost:4173.");
+
+const routes = {
+  "/:city/:category":	"returns :category list for :city"
+};
+
+app.get("/", (req, res) => {
+  res.status(200).json(routes);
 });
 
-app.get("/api/v1/example", (req, res) => {
-  res.status(200).json({ message: "Success" });
+app.get("/:city/:category", (req, res) => {
+  const city = req.params.city;
+  const category = req.params.category
+  res.json(cities[city][category]) 
+});
+
+app.listen(4173, () => {
+  console.log("Listening on http://localhost:4173.");
 });
