9b867bdb8635c8fcecd03ef123c18cfd2c59cc86
fikretellek <fikretellek@icloud.com>
2024-05-25 14:19:40 +0100

    added new components

---
+++ dom-merge-conflict/tasks/buttons-and-counter/src/main.js
@@ -0,0 +1,10 @@
+const mainComponent = () => {
+  const main = document.createElement("main");
+  main.innerHTML = `
+        <p id="counter" data-testid="counter">0</p>
+        <button id="increment">Increment</button>
+    `;
+  return main;
+};
+
+export default mainComponent;
